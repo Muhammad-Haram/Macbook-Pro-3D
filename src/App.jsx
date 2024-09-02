5e886fffe723b269a26069e8e3d5da1819d10017
Muhammad-Haram <haramh643@gmail.com>
2024-09-02 15:31:44 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./style.css";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import MacBook from "./MacBook";
+import AboutUs from "./Components/AboutUs";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="w-full h-screen">
+        <div className="navbar flex gap-10 line absolute top-0 left-1/2 -translate-x-1/2 pt-10 pb-3 text-white">
+          {[
+            "iPhone",
+            "iPad",
+            "services",
+            "ios",
+            "mac",
+            "products",
+            "iPhone",
+            "iPad",
+            "services",
+            "ios",
+            "mac",
+            "products",
+          ].map((e) => {
+            return (
+              <a href="" className="text-white font-[400] text-sm">
+                {e}
+              </a>
+            );
+          })}
+        </div>
 
-export default App
+        <div className="absolute flex flex-col items-center text-white top-32 left-1/2 -translate-x-1/2">
+          <h1 className="masked text-7xl tracking-tighter font-[700]">
+            macbook pro.
+          </h1>
+          <h5>Oh so pro !</h5>
+          <p className="text-center w-3/4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perferendis voluptas veritatis nobis sunt.
+          </p>
+        </div>
+
+        <Canvas camera={{ fov: 20, position: [0, -10, 150] }}>
+          <OrbitControls enableZoom={false} />
+          <Environment
+            files={[
+              "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+            ]}
+          />
+
+          <ScrollControls pages={3}>
+            <MacBook />
+          </ScrollControls>
+        </Canvas>
+      </div>
+
+      <AboutUs />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='w-full h-screen bg-white'>
+      1
+    </div>
+  )
+}
+
+export default AboutUs
